01e284adb066e0f7605518bd3003bfda032c4a69
BobRazoswki <bobongithub@gmail.com>
2014-08-06 16:01:17 +0100

    API set up, js translate to json ok

---
+++ public/jQuery/getburger.js
@@ -0,0 +1,14 @@
+function showRestaurant(restaurant) {
+	console.log(restaurant)
+}
+
+// $( document ).ready(function() {
+   
+// 	// $('#getfood').on('submit', function(event) {
+// 	// 	event.preventDefault;
+// 	// 	var url = "http://api.yelp.com/v2/search?term=food&amp;location=San+Francisco"
+// 	// });
+
+
+
+// });
---
+++ server.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+require 'yelp'
+
+class Yelpy < Sinatra::Base
+
+	set :views, Proc.new { File.join(root, "views") }
+	set :public_folder, Proc.new { File.join(root, "public") }
+	
+	get '/' do
+		client = Yelp::Client.new({ consumer_key: "87W8_iswak1PkyuVaW3Hdg",
+                            consumer_secret: "eMxki9ls2zG7OsZOKgQJ6gjJYeA",
+                            token: "ldMcpmVFzYRsRYXTnOsHoawDx9maluHA",
+                            token_secret: "KooVySQkDcxxrB4RFSfPqXK4t-8"
+                          })
+
+		if params[:search]
+			@results = client.search('London', term: params[:search])
+		end
+
+    erb :index
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+
+
+end
